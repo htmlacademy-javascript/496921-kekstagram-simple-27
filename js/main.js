c7d8ac52ddd517d2ffe9143c0f9bdff914c9d68b
Vadim Tiuriumin <owlriks@gmail.com>
2023-05-01 22:10:30 +0800

    Отрефакторил код

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-const PHOTO_COUNT = 25;
 const PHOTO_DESCRIPTIONS = [
   'На вечеринке с друзьями',
   'Мой кот и я',
@@ -8,35 +7,31 @@ const PHOTO_DESCRIPTIONS = [
   'Жму руку Гению',
   'Моя ласточка'
 ];
+const PHOTO_COUNT = 25;
 
-const photoData = [];
-
-const getRandomInt = (min = 0, max = 1000) => {
-  if (min < 0 || max <= min) {
+const getRandomPositiveInteger = (a = 0, b = 1000) => {
+  if (a < 0 || b < 0) {
     return NaN;
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
 
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
 };
 
-const checkStringMaxLength = (str = 'foo', maxLength = 100) => str.length <= maxLength;
+const checkStringLength = (str = 'Мяу', maxLength = 100) => str.length <= maxLength;
+checkStringLength('bar', 150);
 
-const createPhotoItem = (index) => ({
+const createPhoto = (index) => ({
   id: index,
   url: `photos/${index}.jpg`,
-  description: PHOTO_DESCRIPTIONS[getRandomInt(0, PHOTO_DESCRIPTIONS.length - 1)],
-  likes: getRandomInt(15, 200),
-  comments: getRandomInt(0, 200)
+  description: PHOTO_DESCRIPTIONS[getRandomPositiveInteger(0, PHOTO_DESCRIPTIONS.length - 1)],
+  likes: getRandomPositiveInteger(15, 200),
+  comments: getRandomPositiveInteger(0, 200)
 });
 
-const createPhotoData = () => {
-  for (let i = 1; i <= PHOTO_COUNT; i++) {
-    photoData.push(createPhotoItem(i));
-  }
-};
+const getPhotos = () => Array.from({length: PHOTO_COUNT}, (_item, index) => createPhoto(index + 1));
 
-createPhotoData();
-checkStringMaxLength('bar', 150);
+getPhotos();
+
